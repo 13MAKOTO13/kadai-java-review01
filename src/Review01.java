921ddb36f2d1cef248160c53eb66feb7323e94df
ogjop <ogjop@DESKTOP-S39I1BT>
2023-02-07 15:23:31 +0900

    課題提出

--- src/Review01.java
+++ src/Review01.java
@@ -2,8 +2,14 @@
 public class Review01 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		int price= 2000;
+        double intax =gettax(2000,10);
 
+	System.out.println(price + "円の税込価格は"+((int)(intax+price))+"（消費税は"+ (int)intax + "円）です。");
 	}
 
+	public static double gettax(double num1, double num2){
+		    double result = num1*(num2/100);
+		    return result;
+}
 }
---
+++ src/UserVariable.java
@@ -0,0 +1,11 @@
+public class UserVariable{
+	public static void main(String[] args) {
+		int number2;
+		String str2;
+		number2 = 100;
+		str2
+		= "xyz";
+		System.out.println(100);
+	    System.out.println("xyz");
+	}
+}
