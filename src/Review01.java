50bb6a9c8658b47583706cc88fb75f19c0aac63d
ogjop <ogjop@DESKTOP-S39I1BT>
2023-02-07 22:28:46 +0900

    javaプログラミング入門課題再提出

--- src/Review01.java
+++ src/Review01.java
@@ -2,13 +2,14 @@
 public class Review01 {
 
 	public static void main(String[] args) {
-		int price= 2000;
-        double intax =gettax(2000,10);
+		int price= 1000;
+		int tax = 10;
+        double intax =tax(price,tax);
 
 	System.out.println(price + "円の税込価格は"+((int)(intax+price))+"（消費税は"+ (int)intax + "円）です。");
 	}
 
-	public static double gettax(double num1, double num2){
+	public static double tax(double num1, double num2){
 		    double result = num1*(num2/100);
 		    return result;
 }
